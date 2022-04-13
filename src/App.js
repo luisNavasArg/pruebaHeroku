3d9080ab20d428af1cd813af560deab375ad3221
luisNavasArg <lpipnavas@gmail.com>
2022-04-13 13:57:56 -0300

    listo clase 20

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useEffect,useState}from 'react'
 function App() {
+  const [opcion,setOpcion]=useState('paquetes');
+  const [data,setData]=useState([])
+
+  useEffect(()=>{
+    const fecthData= async()=>{
+      const response = await fetch('https://pruebagcd.herokuapp.com/'+ opcion)
+      const json = await response.json();
+      setData(json)
+    }
+    fecthData();
+  },[opcion])
+  const cambiarOpcion=()=>{
+    if(opcion==='hoteles'){
+      setOpcion('paquetes')
+    }else setOpcion('hoteles')
+
+  }
   return (
     <div className="App">
-     
+      <button onClick={cambiarOpcion}>Cambiar Opción</button>
+      <ul>
+      {data.map((o,i)=>{return <li key={i.toString()+ opcion} >{o.nombre}</li>})}
+      </ul>
     </div>
   );
 }
